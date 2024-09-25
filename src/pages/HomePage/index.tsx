7a9bb6d4804a7ebba7c8c78c6e885c18ad0600c1
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-09-25 20:35:00 -0300

    commit 5

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -3,6 +3,8 @@ import Container from '../../components/Container';
 import { Botao } from '../../components/Botao';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import Entypo from '@expo/vector-icons/Entypo';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 
 export default function HomePage() {
   return (
@@ -21,13 +23,28 @@ export default function HomePage() {
           <AntDesign name="right" size={20} color="black" />
         </TouchableOpacity>
         <Text>R$ 1000,00</Text>
-        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-          <Text>{"["}Pix{"]"}</Text>
-          <Text>{"["}Pagar{"]"}</Text>
-          <Text>{"["}Pegar empréstimo{"]"}</Text>
-          <Text>{"["}Recarga de celular{"]"}</Text>
-          <TouchableOpacity>
-            <Entypo name="box" size={24} color="black" />
+        <ScrollView horizontal showsHorizontalScrollIndicator={false} style={{ marginTop: 10 }}>
+          <TouchableOpacity style={{ alignItems: "center", marginRight: 10 }}>
+            <MaterialCommunityIcons name="cellphone-arrow-down" size={24} color="white" style={{ padding: 20, backgroundColor: "gray", borderRadius: 100 }} />
+            <Text>Área Pix e</Text>
+            <Text>Transferir</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ alignItems: "center", marginRight: 10 }}>
+            <AntDesign name="barcode" size={24} color="white" style={{ padding: 20, backgroundColor: "gray", borderRadius: 100 }} />
+            <Text>Pagar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ alignItems: "center" }}>
+            <FontAwesome6 name="circle-dollar-to-slot" size={24} color="white" style={{ padding: 20, backgroundColor: "gray", borderRadius: 100 }} />
+            <Text>Pegar</Text>
+            <Text>empréstimo</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ alignItems: "center" }}>
+            <MaterialCommunityIcons name="cellphone" size={24} color="white" style={{ padding: 20, backgroundColor: "gray", borderRadius: 100 }} />
+            <Text>Recarga de </Text>
+            <Text>celular</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ alignItems: "center" }}>
+            <Entypo name="box" size={24} color="white" style={{ padding: 20, backgroundColor: "gray", borderRadius: 100 }} />
             <Text>Caixinhas e</Text>
             <Text>Investir</Text>
           </TouchableOpacity>
@@ -40,22 +57,40 @@ export default function HomePage() {
           marginTop={10}
         />
       </View>
-      <Text>-------------------------</Text>
-      <TouchableOpacity style={{ flexDirection: "row", justifyContent: "space-between" }}>
-        <Text style={{ fontSize: 20 }}>Cartao de crédito</Text>
-        <AntDesign name="right" size={20} color="black" />
-      </TouchableOpacity>
-      <Text>Fatura atual</Text>
-      <Text>R$ 1000,00</Text>
-      <Text>Limite disponivel</Text>
-      <Text>R$ 5000,00</Text>
-      <Text>{"["}Aliviar fatura{"]"}</Text>
-      <Text>-------------------------</Text>
-      <TouchableOpacity style={{ flexDirection: "row", justifyContent: "space-between" }}>
-        <Text style={{ fontSize: 20 }}>Empréstimo</Text>
-        <AntDesign name="right" size={20} color="black" />
-      </TouchableOpacity>
-      <Text>R$ 100000,00</Text>
+      <View style={{
+        borderBottomColor: "black",
+        borderBottomWidth: 1,
+        width: "100%",
+        paddingVertical: 10
+      }}>
+        <TouchableOpacity style={{ flexDirection: "row", justifyContent: "space-between" }}>
+          <Text style={{ fontSize: 20 }}>Cartao de crédito</Text>
+          <AntDesign name="right" size={20} color="black" />
+        </TouchableOpacity>
+        <Text>Fatura atual</Text>
+        <Text>R$ 1000,00</Text>
+        <Text>Limite disponivel</Text>
+        <Text>R$ 5000,00</Text>
+        <Botao
+          backgroundColor="blue"
+          textColor="white"
+          onPress={() => { }}
+          label="Aliviar fatura"
+          marginTop={10}
+        />
+      </View>
+      <View style={{
+        borderBottomColor: "black",
+        borderBottomWidth: 1,
+        width: "100%",
+        paddingVertical: 10
+      }}>
+        <TouchableOpacity style={{ flexDirection: "row", justifyContent: "space-between" }}>
+          <Text style={{ fontSize: 20 }}>Empréstimo</Text>
+          <AntDesign name="right" size={20} color="black" />
+        </TouchableOpacity>
+        <Text>R$ 100000,00</Text>
+      </View>
     </Container>
   );
 }
